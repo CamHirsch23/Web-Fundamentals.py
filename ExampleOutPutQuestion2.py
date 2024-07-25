e97239f8e968439dc622ae7650b951f42931e03a
CamHirsch23 <hirschboeck13@gmail.com>
2024-07-25 11:57:32 -0500

    Create ExampleOutPutQuestion2.py

---
+++ ExampleOutPutQuestion2.py
@@ -0,0 +1,9 @@
+Planet: Uranus, Mass: 8.68127, Orbit Period: 30685.4 days
+Planet: Neptune, Mass: 1.02413, Orbit Period: 60189.0 days
+Planet: Jupiter, Mass: 1.89819, Orbit Period: 4332.589 days
+Planet: Mars, Mass: 6.41712, Orbit Period: 686.98 days
+Planet: Mercury, Mass: 3.30114, Orbit Period: 87.969 days
+Planet: Saturn, Mass: 5.68336, Orbit Period: 10759.22 days
+Planet: Earth, Mass: 5.97237, Orbit Period: 365.256 days
+Planet: Venus, Mass: 4.86747, Orbit Period: 224.701 days
+The heaviest planet is Uranus with a mass of 8.68127 kg.
