faf34f93456c00ec74621c11b143e8aed13c2074
CamHirsch23 <hirschboeck13@gmail.com>
2024-07-25 11:37:46 -0500

    Create ExampleOutPut.py

---
+++ ExampleOutPut.py
@@ -0,0 +1,4 @@
+Name: Pikachu, Abilities: ['static', 'lightning-rod'], Weight: 60
+Name: Bulbasaur, Abilities: ['overgrow', 'chlorophyll'], Weight: 69
+Name: Charmander, Abilities: ['blaze', 'solar-power'], Weight: 85
+Average Weight: 71.33333333333333
