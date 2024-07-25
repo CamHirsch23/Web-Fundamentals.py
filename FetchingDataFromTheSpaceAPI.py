91240299fb4bc8fecc4e806bb0d8f1255e082617
CamHirsch23 <hirschboeck13@gmail.com>
2024-07-25 11:55:23 -0500

    Create FetchingDataFromTheSpaceAPI.py

---
+++ FetchingDataFromTheSpaceAPI.py
@@ -0,0 +1,19 @@
+import requests
+
+def fetch_planet_data():
+    """
+    Fetches data from the Space API and processes it to extract information about planets.
+    """
+    url = "https://api.le-systeme-solaire.net/rest/bodies/"
+    response = requests.get(url)
+    planets = response.json()['bodies']
+
+    # Process each planet's info
+    for planet in planets:
+        if planet['isPlanet']:
+            name = planet['englishName']
+            mass = planet['mass']['massValue'] if planet['mass'] else 'N/A'
+            orbit_period = planet['sideralOrbit'] if planet['sideralOrbit'] else 'N/A'
+            print(f"Planet: {name}, Mass: {mass}, Orbit Period: {orbit_period} days")
+
+fetch_planet_data()
