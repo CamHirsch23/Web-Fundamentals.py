70b85098492d6c3339b4ad88986cd9eaabd3fce7
CamHirsch23 <hirschboeck13@gmail.com>
2024-07-25 11:54:36 -0500

    Create SetUpExploringTheDigitalCosmosWithPythonAndTheWeb.py

---
+++ SetUpExploringTheDigitalCosmosWithPythonAndTheWeb.py
@@ -0,0 +1,27 @@
+Steps:
+
+Create a new Python virtual environment:
+
+bash
+
+python -m venv spaceenv
+
+Activate the virtual environment:
+
+On Windows:
+
+bash
+
+spaceenv\Scripts\activate
+
+On macOS/Linux:
+
+bash
+
+source spaceenv/bin/activate
+
+Install the requests package:
+
+bash
+
+pip install requests
