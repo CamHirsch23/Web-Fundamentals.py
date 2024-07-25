e1d66b0cb7cac55e62e577a021c9d3821d74dcca
CamHirsch23 <hirschboeck13@gmail.com>
2024-07-25 10:37:54 -0500

    Create SettingUpAPythonVirtualEnvironmentAndInstallingPackages.py

---
+++ SettingUpAPythonVirtualEnvironmentAndInstallingPackages.py
@@ -0,0 +1,19 @@
+Steps:
+
+Create a new Python virtual environment:
+bash
+python -m venv myenv
+
+Activate the virtual environment:
+On Windows:
+
+bash
+myenv\Scripts\activate
+On macOS/Linux:
+
+bash
+source myenv/bin/activate
+
+Install the requests package:
+bash
+pip install requests
